b43b0fed6a8a8d73d41775ffb861fde23679f2d4
picoHz <53327867+picoHz@users.noreply.github.com>
2024-09-09 20:16:26 +0900

    Communicate server with serializable channel

--- kodecks-bevy/src/scene/game/server.rs
+++ kodecks-bevy/src/scene/game/server.rs
@@ -1,60 +1,73 @@
 use crate::scene::GlobalState;
 use bevy::{ecs::world::Command, prelude::*};
-use bevy_async_task::{AsyncTaskRunner, AsyncTaskStatus};
-use futures::channel::mpsc::{Receiver, Sender};
-use kodecks::{
-    action::{Action, PlayerAvailableActions},
-    env::LocalEnvironment,
-    game::Game,
-    log::LogAction,
-    player::PlayerId,
-    profile::GameProfile,
-};
-use kodecks_bot::{Bot, DefaultBot};
-use kodecks_catalog::CATALOG;
-use serde_tuple::{Deserialize_tuple, Serialize_tuple};
+use futures::channel::mpsc::Receiver;
+use kodecks::{action::Action, game::LocalGameState, player::PlayerId, profile::GameProfile};
+use kodecks_server::message::{self, Input};
+use std::sync::Mutex;
 
 pub struct ServerPlugin;
 
 impl Plugin for ServerPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<ServerEvent>()
-            .add_systems(OnEnter(GlobalState::GameInit), init)
+            .init_resource::<Server>()
             .add_systems(OnEnter(GlobalState::GameCleanup), cleanup)
-            .add_systems(Update, recv_events.run_if(resource_exists::<ServerChannel>))
-            .add_systems(
-                Update,
-                run_server
-                    .run_if(resource_exists::<Server>.and_then(resource_exists::<ServerChannel>)),
-            );
+            .add_systems(Update, recv_events);
     }
 }
 
-fn init(mut commands: Commands) {
-    commands.insert_resource(ServerChannel::default());
-}
-
 fn cleanup(mut commands: Commands) {
-    commands.remove_resource::<ServerChannel>();
+    commands.remove_resource::<Session>();
 }
 
 #[derive(Resource)]
 pub struct Server {
-    game: Game,
-    available_actions: Option<PlayerAvailableActions>,
-    user_player: PlayerId,
-    player_in_action: PlayerId,
+    server: kodecks_server::Server,
+    event_recv: Receiver<kodecks_server::message::Output>,
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        let (event_send, event_recv) = futures::channel::mpsc::channel(256);
+        let event_send = Mutex::new(event_send);
+        let server = kodecks_server::Server::new(move |event| {
+            event_send.lock().unwrap().try_send(event).unwrap();
+        });
+        Self { server, event_recv }
+    }
 }
 
 impl Server {
-    pub fn new(profile: GameProfile) -> Self {
-        let game = Game::new(profile, &CATALOG);
-        let player_in_action = game.env().state.players.player_in_turn().id;
-        Self {
-            game,
-            available_actions: None,
-            user_player: PlayerId::new("player1"),
-            player_in_action,
+    pub fn create_session(&mut self, profile: GameProfile) {
+        self.server
+            .handle_input(Input::Command(message::Command::CreateSession { profile }));
+    }
+}
+
+#[derive(Resource)]
+struct Session {
+    id: u32,
+    player: PlayerId,
+}
+
+fn recv_events(
+    mut commands: Commands,
+    mut server: ResMut<Server>,
+    mut events: EventWriter<ServerEvent>,
+) {
+    while let Ok(Some(event)) = server.event_recv.try_next() {
+        match event {
+            message::Output::SessionEvent(event) => match event.event {
+                message::SessionEventKind::Created => {
+                    commands.insert_resource(Session {
+                        id: event.session,
+                        player: event.player,
+                    });
+                }
+                message::SessionEventKind::GameUpdated { state } => {
+                    events.send(ServerEvent(state));
+                }
+            },
         }
     }
 }
@@ -63,105 +76,20 @@ pub struct SendCommand(pub Action);
 
 impl Command for SendCommand {
     fn apply(self, world: &mut World) {
-        if let Some(mut channel) = world.get_resource_mut::<ServerChannel>() {
-            let _ = channel.cmd_send.try_send(self.0);
-        }
-    }
-}
-
-#[derive(Event, Clone, Serialize_tuple, Deserialize_tuple)]
-pub struct ServerEvent {
-    pub env: LocalEnvironment,
-    pub logs: Vec<LogAction>,
-    pub available_actions: Option<PlayerAvailableActions>,
-}
-
-#[derive(Resource)]
-struct ServerChannel {
-    cmd_send: Sender<Action>,
-    cmd_recv: Receiver<Action>,
-    event_send: Sender<ServerEvent>,
-    event_recv: Receiver<ServerEvent>,
-}
-
-impl Default for ServerChannel {
-    fn default() -> Self {
-        let (cmd_send, cmd_recv) = futures::channel::mpsc::channel(256);
-        let (event_send, event_recv) = futures::channel::mpsc::channel(256);
-        Self {
-            cmd_send,
-            cmd_recv,
-            event_send,
-            event_recv,
-        }
-    }
-}
-
-fn recv_events(mut channel: ResMut<ServerChannel>, mut events: EventWriter<ServerEvent>) {
-    while let Ok(Some(event)) = channel.event_recv.try_next() {
-        events.send(event);
-    }
-}
-
-fn run_server(
-    mut server: ResMut<Server>,
-    mut channel: ResMut<ServerChannel>,
-    mut runner: AsyncTaskRunner<Option<Action>>,
-) {
-    let mut action = None;
-
-    if server
-        .available_actions
-        .as_ref()
-        .map_or(false, |actions| !actions.actions.is_empty())
-    {
-        if server.player_in_action == server.user_player {
-            if let Ok(Some(player_action)) = channel.cmd_recv.try_next() {
-                action = Some(player_action);
-            } else {
-                return;
-            }
-        } else {
-            match runner.poll() {
-                AsyncTaskStatus::Finished(bot_action) => {
-                    action = bot_action;
-                }
-                AsyncTaskStatus::Idle => {}
-                _ => {
-                    return;
-                }
+        if let Some(session) = world.get_resource::<Session>() {
+            let id = session.id;
+            let player = session.player;
+            if let Some(mut conn) = world.get_resource_mut::<Server>() {
+                conn.server
+                    .handle_input(Input::SessionCommand(message::SessionCommand {
+                        session: id,
+                        player,
+                        kind: message::SessionCommandKind::NextAction { action: self.0 },
+                    }));
             }
         }
     }
-
-    let player = server.player_in_action;
-    let report = server.game.tick(player, action);
-    server
-        .available_actions
-        .clone_from(&report.available_actions);
-
-    if let Some(available_actions) = &report.available_actions {
-        server.player_in_action = available_actions.player;
-    }
-
-    if let Some(actions) = &report.available_actions {
-        if actions.player != server.user_player && !actions.actions.is_empty() {
-            let env = server.game.env().clone();
-            let report = report.clone();
-            if let Some(available_actions) = report.available_actions {
-                runner.start(async move {
-                    let mut bot = DefaultBot::builder().build();
-                    bot.compute_best_action(env, &available_actions)
-                });
-            }
-        }
-    }
-
-    let _ = channel.event_send.try_send(ServerEvent {
-        env: server.game.env().local(server.user_player),
-        logs: report.logs,
-        available_actions: report
-            .available_actions
-            .filter(|actions| actions.player == server.user_player),
-    });
 }
+
+#[derive(Event, Clone, Deref)]
+pub struct ServerEvent(LocalGameState);
---
+++ kodecks-server/src/lib.rs
@@ -0,0 +1,55 @@
+use kodecks::profile::GameProfile;
+use message::{Command, Input};
+use session::Session;
+use std::{collections::HashMap, sync::Arc};
+
+pub mod message;
+pub mod session;
+
+pub type ServerCallback = dyn Fn(message::Output) + Send + Sync + 'static;
+
+pub struct Server {
+    session_counter: u32,
+    sessions: HashMap<u32, Session>,
+    callback: Arc<Box<ServerCallback>>,
+}
+
+impl Server {
+    pub fn new<F>(callback: F) -> Self
+    where
+        F: Fn(message::Output) + Send + Sync + 'static,
+    {
+        Self {
+            session_counter: 0,
+            sessions: HashMap::new(),
+            callback: Arc::new(Box::new(callback)),
+        }
+    }
+
+    pub fn handle_input(&mut self, input: message::Input) {
+        match input {
+            Input::Command(command) => match command {
+                Command::CreateSession { profile } => {
+                    self.create_session(profile);
+                }
+            },
+            message::Input::SessionCommand(session_command) => {
+                let id = session_command.session;
+                if let Some(session) = self.sessions.get_mut(&id) {
+                    session.process_command(session_command);
+                    if session.is_ended() {
+                        self.sessions.remove(&id);
+                    }
+                }
+            }
+        }
+    }
+
+    fn create_session(&mut self, profile: GameProfile) {
+        let session_id = self.session_counter;
+        self.session_counter += 1;
+
+        let session = Session::new(session_id, profile, self.callback.clone());
+        self.sessions.insert(session_id, session);
+    }
+}
