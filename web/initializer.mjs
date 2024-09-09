669c0065370bece294fb2a05741e4b129d3455d9
picoHz <53327867+picoHz@users.noreply.github.com>
2024-09-09 22:53:01 +0900

    Update estimated wasm binary size

--- web/initializer.mjs
+++ web/initializer.mjs
@@ -1,10 +1,12 @@
+const estimatedSize = 32850000;
+
 export default function myInitializer () {
   return {
     onStart: () => {
       document.getElementById('loading-screen').style.display = 'flex';
     },
     onProgress: ({current, total}) => {
-      const totalSize = total || 57600000;
+      const totalSize = total || estimatedSize;
       const progress = Math.min(Math.round((current / totalSize) * 100), 100);
       document.querySelector('#loading-percentage').textContent = `${progress}%`;
     },
