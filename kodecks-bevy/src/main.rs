012b709f35163815974f0e6d163b8b2e4e160f4b
picoHz <53327867+picoHz@users.noreply.github.com>
2024-09-11 20:03:42 +0900

    Set minimum window size

--- kodecks-bevy/src/main.rs
+++ kodecks-bevy/src/main.rs
@@ -16,6 +16,11 @@ fn main() {
                 title: "Kodecks".into(),
                 window_theme: Some(WindowTheme::Dark),
                 fit_canvas_to_parent: true,
+                resize_constraints: WindowResizeConstraints {
+                    min_width: 1100.0,
+                    min_height: 700.0,
+                    ..default()
+                },
                 ..default()
             }),
             ..default()
