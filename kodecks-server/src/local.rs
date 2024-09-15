fa850d957e9dcdf207202253badcdafb73a284f2
picoHz <53327867+picoHz@users.noreply.github.com>
2024-09-15 19:06:24 +0900

    LocalServer: Run Server on a separate thread

--- kodecks-server/src/local.rs
+++ kodecks-server/src/local.rs
@@ -6,24 +6,33 @@ use futures::channel::mpsc::{self, Receiver};
 use std::sync::Mutex;
 
 pub struct LocalServer {
-    server: Server,
+    command_send: std::sync::mpsc::Sender<Input>,
     event_recv: Receiver<Output>,
 }
 
 impl Default for LocalServer {
     fn default() -> Self {
+        let (command_send, command_recv) = std::sync::mpsc::channel();
         let (event_send, event_recv) = mpsc::channel(256);
         let event_send = Mutex::new(event_send);
-        let server = Server::new(move |event| {
-            event_send.lock().unwrap().try_send(event).unwrap();
+        std::thread::spawn(move || {
+            let mut server = Server::new(move |event| {
+                event_send.lock().unwrap().try_send(event).unwrap();
+            });
+            while let Ok(input) = command_recv.recv() {
+                server.handle_input(input);
+            }
         });
-        Self { server, event_recv }
+        Self {
+            command_send,
+            event_recv,
+        }
     }
 }
 
 impl Connection for LocalServer {
     fn send(&mut self, input: Input) {
-        self.server.handle_input(input);
+        self.command_send.send(input).unwrap();
     }
 
     fn recv(&mut self) -> Option<Output> {
