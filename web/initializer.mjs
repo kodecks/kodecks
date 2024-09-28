d07a27ac2b448fc6584f4e95fa11799edefbe878
picoHz <53327867+picoHz@users.noreply.github.com>
2024-09-28 16:57:04 +0900

    Update estimatedSize in myInitializer function

--- web/initializer.mjs
+++ web/initializer.mjs
@@ -1,11 +1,11 @@
-const estimatedSize = 15800000;
+const estimatedSize = 16200000;
 
-export default function myInitializer () {
+export default function myInitializer() {
   return {
     onStart: () => {
       document.getElementById('loading-screen').style.display = 'flex';
     },
-    onProgress: ({current, total}) => {
+    onProgress: ({ current, total }) => {
       const totalSize = total || estimatedSize;
       const progress = Math.min(Math.round((current / totalSize) * 100), 100);
       document.querySelector('#loading-percentage').textContent = `${progress}%`;
@@ -16,7 +16,7 @@ export default function myInitializer () {
         document.getElementById('loading-screen').style.display = 'none';
       }, 100);
     },
-    onSuccess: (wasm) => {},
-    onFailure: (error) => {}
+    onSuccess: (wasm) => { },
+    onFailure: (error) => { }
   }
 };
