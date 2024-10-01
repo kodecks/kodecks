b402620a69497f434660015ac47a55e5c4530709
picoHz <53327867+picoHz@users.noreply.github.com>
2024-10-01 18:51:11 +0900

    kodecks-engine: Remove serde

--- kodecks-engine/src/message.rs
+++ kodecks-engine/src/message.rs
@@ -1,51 +1,43 @@
 use bincode::{Decode, Encode};
 use kodecks::{action::Action, deck::DeckList, env::LocalGameState, profile::GameProfile};
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
-#[serde(tag = "type", rename_all = "snake_case")]
+#[derive(Debug, Clone, Encode, Decode)]
 pub enum Input {
     Command(Command),
     SessionCommand(SessionCommand),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
-#[serde(tag = "command", rename_all = "snake_case")]
+#[derive(Debug, Clone, Encode, Decode)]
 pub enum Command {
     CreateSession { profile: GameProfile },
     StartRandomMatch { deck: DeckList },
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
+#[derive(Debug, Clone, Encode, Decode)]
 pub struct SessionCommand {
     pub session: u32,
     pub player: u8,
-    #[serde(flatten)]
     pub kind: SessionCommandKind,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
-#[serde(tag = "command", rename_all = "snake_case")]
+#[derive(Debug, Clone, Encode, Decode)]
 pub enum SessionCommandKind {
     NextAction { action: Action },
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
-#[serde(tag = "type", rename_all = "snake_case")]
+#[derive(Debug, Clone, Encode, Decode)]
 pub enum Output {
     SessionEvent(SessionEvent),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
+#[derive(Debug, Clone, Encode, Decode)]
 pub struct SessionEvent {
     pub session: u32,
     pub player: u8,
-    #[serde(flatten)]
     pub event: SessionEventKind,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
-#[serde(tag = "event", rename_all = "snake_case")]
+#[derive(Debug, Clone, Encode, Decode)]
 pub enum SessionEventKind {
     Created,
     GameUpdated { state: LocalGameState },
