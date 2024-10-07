099eb95456bbfe0a9ab7d4a2da9a9273163eccd5
picoHz <53327867+picoHz@users.noreply.github.com>
2024-10-07 23:14:43 +0900

    Add <<Wiretap Vine>>

---
+++ kodecks-catalog/src/cards/wiretap_vine.rs
@@ -0,0 +1,15 @@
+use crate::card_def;
+use kodecks::prelude::*;
+
+card_def!(
+    CardDef,
+    "wire",
+    "Wiretap Vine",
+    color: Color::BLUE,
+    cost: 1,
+    card_type: CardType::Creature,
+    creature_type: CreatureType::Program,
+    power: 200,
+);
+
+impl Effect for CardDef {}
