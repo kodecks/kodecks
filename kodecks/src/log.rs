c8f110490378077eac204e8166507d1db1d26c28
picoHz <53327867+picoHz@users.noreply.github.com>
2024-10-19 19:06:36 +0900

    Remove Display trait from LogAction

--- kodecks/src/log.rs
+++ kodecks/src/log.rs
@@ -4,50 +4,53 @@ use crate::{
 };
 use bincode::{Decode, Encode};
 use serde::{Deserialize, Serialize};
-use strum::Display;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Display, Encode, Decode)]
+#[derive(Debug, Clone, Serialize, Deserialize, Encode, Decode)]
 #[serde(rename_all = "snake_case")]
 pub enum LogAction {
-    #[strum(to_string = "Game started")]
     GameStarted,
-    #[strum(to_string = "Game ended with {reason}")]
     GameEnded {
         winner: Option<u8>,
         reason: EndgameReason,
     },
-    #[strum(to_string = "Turn {turn} started for {player}")]
-    TurnChanged { turn: u32, player: u8 },
-    #[strum(to_string = "Phase changed to {phase}")]
-    PhaseChanged { phase: Phase },
-    #[strum(to_string = "Attack declared by {attacker}")]
-    AttackDeclared { attacker: ObjectId },
-    #[strum(to_string = "{attacker} attacks {blocker}")]
+    TurnChanged {
+        turn: u32,
+        player: u8,
+    },
+    PhaseChanged {
+        phase: Phase,
+    },
+    AttackDeclared {
+        attacker: ObjectId,
+    },
     CreatureAttackedCreature {
         attacker: ObjectId,
         blocker: ObjectId,
     },
-    #[strum(to_string = "{attacker} attacks {player}")]
-    CreatureAttackedPlayer { attacker: ObjectId, player: u8 },
-    #[strum(to_string = "Life changed for {player}: {life}")]
-    LifeChanged { player: u8, life: u32 },
-    #[strum(to_string = "Damage {amount} inflicted to {player}")]
-    DamageTaken { player: u8, amount: u32 },
-    #[strum(to_string = "Shards generated to {player} in {color} color: {amount}")]
+    CreatureAttackedPlayer {
+        attacker: ObjectId,
+        player: u8,
+    },
+    LifeChanged {
+        player: u8,
+        life: u32,
+    },
+    DamageTaken {
+        player: u8,
+        amount: u32,
+    },
     ShardsEarned {
         player: u8,
         source: ObjectId,
         color: Color,
         amount: u32,
     },
-    #[strum(to_string = "Shards removed from {player} in {color} color: {amount}")]
     ShardsSpent {
         player: u8,
         source: ObjectId,
         color: Color,
         amount: u32,
     },
-    #[strum(to_string = "Card {card} moved from {from:?} to {to:?} due to {reason:?}")]
     CardMoved {
         player: u8,
         card: ObjectId,
@@ -55,16 +58,24 @@ pub enum LogAction {
         to: PlayerZone,
         reason: MoveReason,
     },
-    #[strum(to_string = "Card Token {card} generated")]
-    CardTokenGenerated { card: ObjectId },
-    #[strum(to_string = "Card Token {card} destroyed")]
-    CardTokenDestroyed { card: ObjectId },
-    #[strum(to_string = "Deck shuffled for {player}")]
-    DeckShuffled { player: u8 },
-    #[strum(to_string = "Effect {id} triggered by {source}")]
-    EffectActivated { source: ObjectId, id: EffectId },
-    #[strum(to_string = "Card {target} targeted by {source}")]
-    CardTargeted { source: ObjectId, target: ObjectId },
-    #[strum(to_string = "Shield broken for {card}")]
-    ShieldBroken { card: ObjectId },
+    CardTokenGenerated {
+        card: ObjectId,
+    },
+    CardTokenDestroyed {
+        card: ObjectId,
+    },
+    DeckShuffled {
+        player: u8,
+    },
+    EffectActivated {
+        source: ObjectId,
+        id: EffectId,
+    },
+    CardTargeted {
+        source: ObjectId,
+        target: ObjectId,
+    },
+    ShieldBroken {
+        card: ObjectId,
+    },
 }
