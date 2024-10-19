daa65c17236f7791361e63b6b6b92f00e61fd237
picoHz <53327867+picoHz@users.noreply.github.com>
2024-10-20 00:05:07 +0900

    Use logos crate for card text parsing

--- kodecks/src/text.rs
+++ kodecks/src/text.rs
@@ -1,7 +1,6 @@
-use regex_lite::Regex;
-use std::sync::LazyLock;
+use logos::Logos;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Section<'a> {
     Text(&'a str),
     Card(&'a str),
@@ -10,29 +9,66 @@ pub enum Section<'a> {
 }
 
 pub fn parse_text(text: &str) -> Vec<Section> {
-    static REGEX: LazyLock<Regex> =
-        LazyLock::new(|| Regex::new(r"(?:<<(.+?)>>)|(?:\[\[(.+?)\]\])|(?:([+-]?[0-9]+))").unwrap());
-
     let mut sections = vec![];
-    let mut cursor = 0;
-    for capture in REGEX.captures_iter(text) {
-        let prefix = &text[cursor..capture.get(0).unwrap().start()];
-        if !prefix.is_empty() {
-            sections.push(Section::Text(prefix));
+    let mut lexer = Token::lexer(text);
+    while let Some(token) = lexer.next() {
+        match token {
+            Ok(Token::StartCardName) => {
+                if let Some(Ok(Token::Text(card))) = lexer.next() {
+                    sections.push(Section::Card(card));
+                }
+                lexer.next();
+            }
+            Ok(Token::StartKeyword) => {
+                if let Some(Ok(Token::Text(keyword))) = lexer.next() {
+                    sections.push(Section::Keyword(keyword));
+                }
+                lexer.next();
+            }
+            Ok(Token::Number(number)) => sections.push(Section::Number(number)),
+            Ok(Token::Text(text)) => sections.push(Section::Text(text)),
+            _ => {}
         }
-        if let Some(card) = capture.get(1) {
-            sections.push(Section::Card(card.as_str()));
-        } else if let Some(ability) = capture.get(2) {
-            sections.push(Section::Keyword(ability.as_str()));
-        } else if let Some(ability) = capture.get(3) {
-            sections.push(Section::Number(ability.as_str().parse().unwrap()));
-        }
-
-        cursor = capture.get(0).unwrap().end();
-    }
-    let suffix = &text[cursor..];
-    if !suffix.is_empty() {
-        sections.push(Section::Text(suffix));
     }
     sections
+}
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos()]
+enum Token<'source> {
+    #[token("<<")]
+    StartCardName,
+    #[token(">>")]
+    EndCardName,
+    #[token("[[")]
+    StartKeyword,
+    #[token("]]")]
+    EndKeyword,
+    #[regex("[+-]?[0-9]+", |lex| lex.slice().parse::<i32>().unwrap())]
+    Number(i32),
+    #[regex("[^+\\-<>\\[\\]0-9]+", |lex| lex.slice())]
+    Text(&'source str),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_text() {
+        let text = "This is a <<card>> with [[ability]] and +500 power.";
+        let sections = parse_text(text);
+        assert_eq!(
+            sections,
+            vec![
+                Section::Text("This is a "),
+                Section::Card("card"),
+                Section::Text(" with "),
+                Section::Keyword("ability"),
+                Section::Text(" and "),
+                Section::Number(500),
+                Section::Text(" power.")
+            ]
+        );
+    }
 }
