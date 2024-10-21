7fa173d7e5f6c04e55ac37cf5ebd914f76db9a36
picoHz <53327867+picoHz@users.noreply.github.com>
2024-10-21 22:48:50 +0900

    Add <<Ruby Digger>>

---
+++ kodecks-catalog/src/cards/ruby_digger.rs
@@ -0,0 +1,15 @@
+use crate::card_def;
+use kodecks::prelude::*;
+
+card_def!(
+    CardDef,
+    "ruby",
+    "Ruby Digger",
+    color: Color::RED,
+    cost: 2,
+    card_type: CardType::Creature,
+    creature_type: CreatureType::Robot,
+    power: 300,
+);
+
+impl Effect for CardDef {}
